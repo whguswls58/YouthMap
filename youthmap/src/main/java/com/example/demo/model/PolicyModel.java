c81b6ea5f38a23b6a9cf6e61775644f478b0eb9b
whguswls58 <whguwls58@naver.com>
2025-06-02 18:36:39 +0900

    환경설정 업데이트

---
+++ youthmap/src/main/java/com/example/demo/model/PolicyModel.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+@Data
+@Alias("policy")
+public class PolicyModel {
+
+}
---
+++ youthmap/src/main/java/com/example/demo/service/PolicyService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.PolicyDao;
+
+
+@Service
+public class PolicyService {
+	
+	private PolicyDao dao;
+	
+}
