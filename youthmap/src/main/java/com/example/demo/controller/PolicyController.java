a368cc59e0209b4ced5fc8f6ec583655b7b1f3c1
whguswls58 <whguwls58@naver.com>
2025-06-09 19:58:55 +0900

    api 데이터 추출후 정제중

--- youthmap/src/main/java/com/example/demo/controller/PolicyController.java
+++ youthmap/src/main/java/com/example/demo/controller/PolicyController.java
@@ -32,8 +32,9 @@ public class PolicyController {
 	@RequestMapping("/")
 	public String test() throws Exception{
         
-        String jsonData = service.getYouthPolicies();		// service 클래스에 정책 정보 요청 
-        System.out.println(jsonData);
+		// 총 정책수
+        String jsonData = service.getYouthPolicies(1, 1);		// service 클래스에 정책 정보 요청 
+        System.out.println("총 정책수 체크");
         
         // JSON 파싱
         JSONParser parser = new JSONParser();
@@ -41,30 +42,50 @@ public class PolicyController {
         JSONObject dataObject = (JSONObject) jsonObject.get("result");
         
         JSONObject pagging = (JSONObject)dataObject.get("pagging");
-        System.out.println(pagging.get("totCount"));
+        int totCount = ((Long) pagging.get("totCount")).intValue();
+        System.out.println(totCount);
+        
+        int pageSize = 20;
+        
+        int pageCount = totCount / pageSize + ((totCount % pageSize == 0) ? 0 : 1);
         
         List<PolicyModel> pm = new ArrayList<PolicyModel>();
         
-        // JSON에서 정책 데이터 추출
-        JSONArray plycList = (JSONArray)dataObject.get("youthPolicyList");
-        
-        for( Object obj : plycList) {
-        	JSONObject policy = (JSONObject) obj;
-        	PolicyModel plcyMd = new PolicyModel();
+        for(int pageNum=1 ; pageNum < 3 ; pageNum++) {
+        	System.out.println("=====================================================");
+        	System.out.println("page 번호 : " + pageNum);
         	
-        	if(((String)policy.get("rgtrHghrkInstCdNm")).equals("서울특별시")) {
+        	jsonData = service.getYouthPolicies(pageNum, pageSize);		// service 클래스에 정책 정보 요청 
+        	System.out.println(jsonData);
+        	
+        	// JSON 파싱
+        	parser = new JSONParser();
+        	jsonObject = (JSONObject) parser.parse(jsonData);
+        	dataObject = (JSONObject) jsonObject.get("result");
+        	
+        	// JSON에서 정책 데이터 추출
+        	JSONArray plycList = (JSONArray)dataObject.get("youthPolicyList");
+        	
+        	for( Object obj : plycList) {
+        		JSONObject policy = (JSONObject) obj;
+        		PolicyModel plcyMd = new PolicyModel();
         		
-        		plcyMd.setPlcy_no((String)policy.get("plcyNo"));
-        		plcyMd.setPlcy_nm((String)policy.get("plcyNm"));
-        		plcyMd.setPlcy_expln_cn((String)policy.get("plcyExplnCn"));
-        		plcyMd.setPlcy_kywd_nm((String)policy.get("plcyKywdNm"));
-        		plcyMd.setLclsf_nm((String)policy.get("lclsfNm"));
-        		plcyMd.setMclsf_nm((String)policy.get("mclsfNm"));
-        		plcyMd.setPlcy_sprt_cn((String)policy.get("plcySprtCn"));
-        		
-        		pm.add(plcyMd);
+        		if(((String)policy.get("rgtrHghrkInstCdNm")).equals("서울특별시") ||
+        				((String)policy.get("rgtrHghrkInstCdNm")).equals("고용노동부") ||
+        				((String)policy.get("rgtrHghrkInstCdNm")).equals("교육부")) {
+        			
+        			plcyMd.setPlcy_no((String)policy.get("plcyNo"));
+        			plcyMd.setPlcy_nm((String)policy.get("plcyNm"));
+        			plcyMd.setPlcy_expln_cn((String)policy.get("plcyExplnCn"));
+        			plcyMd.setPlcy_kywd_nm((String)policy.get("plcyKywdNm"));
+        			plcyMd.setLclsf_nm((String)policy.get("lclsfNm"));
+        			plcyMd.setMclsf_nm((String)policy.get("mclsfNm"));
+        			plcyMd.setPlcy_sprt_cn((String)policy.get("plcySprtCn"));
+        			plcyMd.setRgtr_hghrk_inst_cd_nm((String)policy.get("rgtrHghrkInstCdNm"));
+        			
+        			pm.add(plcyMd);
+        		}
         	}
-        	
         }
         
         for( PolicyModel obj : pm) {
@@ -76,6 +97,7 @@ public class PolicyController {
         	System.out.println(obj.getLclsf_nm());
         	System.out.println(obj.getMclsf_nm());
         	System.out.println(obj.getPlcy_sprt_cn());
+        	System.out.println(obj.getRgtr_hghrk_inst_cd_nm());
         	
         	System.out.println("---------------------------------------------------------------------");
         }
--- youthmap/src/main/java/com/example/demo/service/PolicyService.java
+++ youthmap/src/main/java/com/example/demo/service/PolicyService.java
@@ -7,6 +7,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.dao.PolicyDao;
 
@@ -17,11 +18,14 @@ public class PolicyService {
 	private PolicyDao dao;
 	
 	
-	public String getYouthPolicies() {
+	public String getYouthPolicies(@RequestParam("pageNum") int pageNum, @RequestParam("size") int pageSize) {
         
 		String apiKey = "ec807d36-bd03-4e9d-92bd-20eec0f42d73";								// 인증키
 		String apiUrl = "https://www.youthcenter.go.kr/go/ythip/getPlcy";					// api url
-        String params = "?apiKeyNm=" + apiKey + "&pageNum=1&pageSize=10&rtnType=json";		// api 요청값
+        String params = "?apiKeyNm=" + apiKey + "&pageNum=" + pageNum + "&pageSize="+ pageSize +"&rtnType=json";		// api 요청값
+        
+        System.out.println(params);
+        
         // 요청 pageNum, pageSize 어떻게 처리해서 모든 데이터를 받아올 것인지?
         try {
             URL url = new URL(apiUrl + params);
