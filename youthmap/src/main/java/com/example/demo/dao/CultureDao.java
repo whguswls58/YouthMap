4944c3a9e5db8275aada1283ec49e00b353a009d
예원 <@DESKTOP-R8FQ81R>
2025-06-18 15:00:36 +0900

    문화 페이지

---
+++ youthmap/src/main/java/com/example/demo/dao/CultureDao.java
@@ -0,0 +1,53 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.model.CultureModel;
+
+@Mapper
+public interface CultureDao {
+
+	int culinsert(CultureModel culMd);
+
+	List<CultureModel> getexhibhition();
+
+	List<CultureModel> getperformance();
+
+	List<CultureModel> getevent();
+
+	// 전시미술
+	List<CultureModel> getexhibhition(CultureModel culture);
+
+	// 전시/미술 전체 리스트
+	List<CultureModel> getexhibitionlist(CultureModel culMd);
+
+	int count(CultureModel culMd);
+
+	CultureModel getexhibhitioncont(CultureModel culMd);
+
+	// 공연
+	int count2(CultureModel culMd);
+	
+	List<CultureModel> getperformancelist(CultureModel culMd);
+
+	CultureModel getperformancecont(CultureModel culMd);
+	
+	// 축제행사
+	List<CultureModel> geteventlist(CultureModel culMd);
+
+	int count3(CultureModel culMd);
+ 
+	CultureModel geteventcont(CultureModel culMd);
+
+	List<CultureModel> searchList(CultureModel culMd);
+
+	int countall(CultureModel culMd);
+
+	List<CultureModel> getallList(CultureModel culMd);
+
+
+
+
+}
