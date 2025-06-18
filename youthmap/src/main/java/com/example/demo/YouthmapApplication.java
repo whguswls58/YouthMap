90979754dadabd5a49196f6995d57cc71dd01b44
whguswls58 <whguwls58@naver.com>
2025-06-18 17:58:43 +0900

    테스트중

--- youthmap/src/main/java/com/example/demo/YouthmapApplication.java
+++ youthmap/src/main/java/com/example/demo/YouthmapApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+
 @SpringBootApplication
 @MapperScan("com.example.demo.dao")
 @EnableScheduling
